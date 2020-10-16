2dda4839889b5c9b97a4bdb466efc9cb8be3f168
crgjr66 <chuckgilbreth@yahoo.com>
2020-10-16 10:10:32 -0700

    Updates

---
+++ precache-manifest.2982d10e33700c5801efc5b45d97fe88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9b1378dd4079bd85df6a7f0bb52565",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "48d4402ee93a034faa3e",
+    "url": "/tic-tac-toe/static/css/main.24caccd7.chunk.css"
+  },
+  {
+    "revision": "e5b6b8900d6016f07c1c",
+    "url": "/tic-tac-toe/static/js/2.99df2a98.chunk.js"
+  },
+  {
+    "revision": "48d4402ee93a034faa3e",
+    "url": "/tic-tac-toe/static/js/main.680efccc.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
